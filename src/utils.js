39f2bbd6bfd0e0f666abafafcfdb4943c78acd78
mydearxym <xieym@chinawyny.com>
2019-12-17 18:40:58 +0800

    chore: bump version, for debug

--- src/utils.js
+++ src/utils.js
@@ -112,3 +112,21 @@ export const getQueryFromUrl = (name, url) => {
 
     return el;
   }
+
+/**
+ * highlight the setting icon in setting panel
+ * @param el {HTMLElement}
+ * @param api editor.js's api
+ *
+ * @returns void
+ * @private
+ */
+
+export const highlightSettingIcon = (el, api) => {
+  if (el.parentNode) {
+    const buttons = el.parentNode.querySelectorAll('.' + api.styles.settingsButton);
+    Array.from(buttons).forEach( button => button.classList.remove(api.styles.settingsButtonActive));
+  }
+
+  el.classList.add(api.styles.settingsButtonActive);
+}
