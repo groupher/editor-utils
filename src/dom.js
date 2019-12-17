49c215b40e5ee46d4c1835b5da7971d7f00e87a6
mydearxym <xieym@chinawyny.com>
2019-12-17 16:07:44 +0800

    feat: add highlightSettingIcon function

---
+++ src/dom.js
@@ -0,0 +1,19 @@
+/**
+ * highlight the setting icon in setting panel
+ * @param el {HTMLElement}
+ * @param api editor.js's api 
+ * 
+ * @returns void
+ * @private
+ */
+
+export const highlightSettingIcon = (el, api) => {
+  if (el.parentNode) {
+    const buttons = el.parentNode.querySelectorAll('.' + api.styles.settingsButton);
+    Array.from(buttons).forEach( button => button.classList.remove(api.styles.settingsButtonActive));
+  }
+
+  el.classList.add(api.styles.settingsButtonActive)
+}
+
+export const holder = 1
