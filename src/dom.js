dc5b06c6d2bbaf24adea1bb1e49efc39adb47d39
mydearxym <mydearxym@qq.com>
2020-02-07 20:20:22 +0800

    feat: add moveCaretToEnd

--- src/dom.js
+++ src/dom.js
@@ -1,19 +1,35 @@
 /**
  * highlight the setting icon in setting panel
  * @param el {HTMLElement}
- * @param api editor.js's api 
- * 
+ * @param api editor.js's api
+ *
  * @returns void
  * @private
  */
 
 export const highlightSettingIcon = (el, api) => {
   if (el.parentNode) {
-    const buttons = el.parentNode.querySelectorAll('.' + api.styles.settingsButton);
-    Array.from(buttons).forEach( button => button.classList.remove(api.styles.settingsButtonActive));
+    const buttons = el.parentNode.querySelectorAll(
+      "." + api.styles.settingsButton
+    );
+    Array.from(buttons).forEach(button =>
+      button.classList.remove(api.styles.settingsButtonActive)
+    );
   }
 
-  el.classList.add(api.styles.settingsButtonActive)
-}
+  el.classList.add(api.styles.settingsButtonActive);
+};
 
-export const holder = 1
+/**
+ * Moves caret to the end of contentEditable element
+ * @param {HTMLElement} element - contentEditable element
+ */
+export const moveCaretToEnd = element => {
+  const range = document.createRange();
+  const selection = window.getSelection();
+
+  range.selectNodeContents(element);
+  range.collapse(false);
+  selection.removeAllRanges();
+  selection.addRange(range);
+};
