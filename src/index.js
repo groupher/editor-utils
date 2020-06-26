2ccb0308a3a0c74d041a26d6a2350c088b216ddc
mydearxym <mydearxym@qq.com>
2020-06-26 18:08:38 +0800

    feat: add enhanceBlock helper

--- src/index.js
+++ src/index.js
@@ -19,3 +19,35 @@ export const TriggerHub = {
   handleInlineMDShortcut,
   handleMention,
 };
+
+/**
+ * enhance the block with options
+ * @param el {HTMLElement}
+ * @param api editor.js's api
+ * @param option enhance options
+ *
+ * @returns void
+ */
+export const enhanceBlock = (el, api, option = {}) => {
+  const opt = Object.assign(
+    { markdown: true, inlineMarkdown: true, mention: true, emoji: true },
+    option
+  );
+
+  api.listeners.on(
+    el,
+    "input",
+    (ev) => {
+      if (opt.markdown) {
+        handleMDShortcut(ev, api);
+      }
+      if (opt.inlineMarkdown) {
+        handleInlineMDShortcut(ev, api);
+      }
+      if (opt.mention) {
+        handleMention(ev);
+      }
+    },
+    true
+  );
+};
