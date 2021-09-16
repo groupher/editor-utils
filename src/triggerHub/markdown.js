254ffc077efbfb3064d692cec9e7cf4da0923f58
mydearxym <mydearxym@qq.com>
2021-09-17 06:59:15 +0800

    refactor(inline-markdown): inline shortcut debug

--- src/triggerHub/markdown.js
+++ src/triggerHub/markdown.js
@@ -242,6 +242,29 @@ export const handleMDShortcut = (ev, api) => {
   }
 };
 
+export const getCharacterPrecedingCaret = (containerEl) => {
+  let precedingChar = "",
+    sel,
+    range,
+    precedingRange;
+  if (window.getSelection) {
+    sel = window.getSelection();
+    if (sel.rangeCount > 0) {
+      range = sel.getRangeAt(0).cloneRange();
+      range.collapse(true);
+      range.setStart(containerEl, 0);
+      precedingChar = range.toString().slice(-1);
+    }
+  } else if ((sel = document.selection) && sel.type != "Control") {
+    range = sel.createRange();
+    precedingRange = range.duplicate();
+    precedingRange.moveToElementText(containerEl);
+    precedingRange.setEndPoint("EndToStart", range);
+    precedingChar = precedingRange.text.slice(-1);
+  }
+  return precedingChar;
+};
+
 /**
  * handle inline markdown syntax like bold, italic, inline-code etc..
  * @return {HTMLDivElement}
@@ -257,6 +280,7 @@ export const handleInlineMDShortcut = (ev, api) => {
   const { isValid, md, html } = _checkInlineMarkdownSyntax(curBlock, ev.data);
   if (!isValid) return;
   console.log("utils: handleInlineMDShortcut isValid, ev: ", ev);
+  console.log("previous char: ", getCharacterPrecedingCaret(ev.target));
 
   if (isValid) {
     const INLINE_MD_HOLDER = `<span id="${ANCHOR.INLINE_MD}" />`;
